c8c68210c659726bfd8e3385101652977d39cd07
Pierre V-F <pierre@pierrevf.consulting>
2025-08-14 16:17:36 +0200

    feat: switching to UV and adding PoC with Mistral

---
+++ main.py
@@ -0,0 +1,45 @@
+"""
+Builds a basic knowledge graph from a file and stores it into Neo4j
+
+Code from:
+https://towardsdatascience.com/enterprise-ready-knowledge-graphs-96028d863e8c/
+"""
+
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_experimental.graph_transformers import LLMGraphTransformer
+from langchain_mistralai import ChatMistralAI
+from langchain_neo4j import Neo4jGraph
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    MISTRAL_API_KEY: str
+
+
+# ------------------- Important --------------------
+llm_transformer = LLMGraphTransformer(
+    llm=ChatMistralAI(temperature=0, model_name="mistral-small")
+)
+# ------------------- Important --------------------
+
+graph = Neo4jGraph(
+    url="neo4j://localhost",
+    database="",
+    username="neo4j",
+    password="your_password",
+)
+
+document = TextLoader(".data/sample.txt").load()
+
+print("Converting document to graph")
+graph_documents = llm_transformer.convert_to_graph_documents(document)
+print("Done")
+
+print(" ")
+
+print("Adding to graph")
+graph.add_graph_documents(graph_documents)
+print("Done")
--- makefile
+++ makefile
@@ -1,15 +1,29 @@
 
-POETRY_VERSION := 1.8.5
-CLI_NAME := "oss4climate.cli"
-
 .PHONY: install
 install:
-	pip install pipx
-	pipx ensurepath
-	pipx install poetry==$(POETRY_VERSION) || echo "Poetry already installed"
-	poetry config virtualenvs.create true 
-	poetry install --no-cache
+	uv sync
 
-	python -m spacy download en_core_web_sm
-	# python -m spacy download en_core_web_trf
+
+.PHONY: local_db_run
+local_db_run:
+	chmod +x src/shell/neo4j_run.sh
+	src/shell/neo4j_run.sh
+
+
+.PHONY: avoid
+avoid:
+	mkdir -p .data/neo4j/data
+	mkdir -p .data/plugins
+
+	pushd /home/pvf/projects/github/kg-oss4climate/.data/neo4j/plugins
+	wget https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/2025.06.0/apoc-2025.06.0-all.jar
+	wget https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.4.0.37/apoc-4.4.0.37-all.jar
+	popd
+
+	docker run \
+		--publish=7474:7474 \
+		--publish=7687:7687 \
+		--volume=.data/neo4j/data:/data \
+		--env NEO4J_AUTH=neo4j/your_password \
+		neo4j:5.26.10
 	
